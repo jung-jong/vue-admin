5b14f62af6fbcb4ee1e788488f83af64a4133d9c
jonghyeon <cjddnjfql@naver.com>
2022-04-17 20:27:37 +0900

    db

--- client/public/api/connect.php
+++ client/public/api/connect.php
@@ -4,10 +4,10 @@ header("Access-Control-Allow-Credentials", "true");
 header('Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 header('Access-Control-Allow-Methods:GET,POST,PUT,DELETE,OPTIONS');
 
-$db_host = "localhost:3307";
-$db_user = "root";
-$db_password="wjdwhdgus1";
-$db_name="test";
+$db_host = "nemolabs.iptime.org";
+$db_user = "nemolabs";
+$db_password="!nemolabs2022";
+$db_name="tongro";
 $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 if (!$conn) {
   die("DB 접속 실패: " . mysqli_connect_error());
