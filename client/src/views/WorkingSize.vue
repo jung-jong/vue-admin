c70cce23767bb248fbbd3a35fa777280f27509d7
jung jonghyeon <cjddnjfql@naver.com>
2022-05-18 16:09:59 +0900

    작업크기 ui

--- client/src/views/WorkingSize.vue
+++ client/src/views/WorkingSize.vue
@@ -10,15 +10,28 @@
               <button class="btn btn-primary" type="button">
                 템플릿 타입 추가
               </button>
-              <button class="btn btn-danger" type="button">❌ 삭제</button>
+              <button class="btn btn-danger ms-auto me-2" type="button">
+                ❌ 삭제
+              </button>
               <button class="btn btn-secondary" type="button">저장</button>
             </div>
             <ul class="list-group">
               <li
+                v-for="(sizeCategory, i) in sizeCategory"
+                :key="i"
+                :class="{ active: i === activeCategory }"
+                @click="activeTemplate(i)"
                 class="list-group-item d-flex justify-content-between align-items-center"
               >
-                프레젠테이션
-                <span class="badge bg-primary rounded-pill">2</span>
+                {{ sizeCategory.TEMPLATE_TYPE_NAME }}
+                <div class="d-flex">
+                  <span role="button" class="material-symbols-rounded">
+                    arrow_upward
+                  </span>
+                  <span role="button" class="material-symbols-rounded">
+                    arrow_downward
+                  </span>
+                </div>
               </li>
             </ul>
           </div>
@@ -26,15 +39,28 @@
           <div class="col">
             <div class="d-flex flex-wrap justify-content-between my-3">
               <button class="btn btn-primary" type="button">분류 추가</button>
-              <button class="btn btn-danger" type="button">❌ 삭제</button>
+              <button class="btn btn-danger ms-auto me-2" type="button">
+                ❌ 삭제
+              </button>
               <button class="btn btn-secondary" type="button">저장</button>
             </div>
             <ul class="list-group">
               <li
+                v-for="(size, i) in size"
+                :key="i"
+                :class="{ active: i === activeSize }"
+                @click="activeSizeName(i)"
                 class="list-group-item d-flex justify-content-between align-items-center"
               >
-                프레젠테이션
-                <span class="badge bg-primary rounded-pill">2</span>
+                {{ size.SIZE_NAME }}
+                <div class="d-flex">
+                  <span role="button" class="material-symbols-rounded">
+                    arrow_upward
+                  </span>
+                  <span role="button" class="material-symbols-rounded">
+                    arrow_downward
+                  </span>
+                </div>
               </li>
             </ul>
           </div>
@@ -43,17 +69,37 @@
             <div
               class="d-flex flex-wrap justify-content-between alingn-items-center my-3"
             >
-              <b class="pt-2">작업크기</b>
+              <h5 class="fw-bold d-flex align-items-end">작업크기</h5>
               <button class="btn btn-secondary" type="button">저장</button>
             </div>
-            <ul class="list-group">
-              <li
-                class="list-group-item d-flex justify-content-between align-items-center"
-              >
-                프레젠테이션
-                <span class="badge bg-primary rounded-pill">2</span>
-              </li>
-            </ul>
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col">
+                    <h5>가로</h5>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div
+                    class="col-1 d-flex align-items-end justify-content-center"
+                  >
+                    <h5>X</h5>
+                  </div>
+                  <div class="col">
+                    <h5>세로</h5>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="col">
+                    <h5>단위</h5>
+                    <select v-model="selected" class="form-select">
+                      <option value="1">px</option>
+                      <option value="2">mm</option>
+                      <option value="3">cm</option>
+                      <option value="4">inch</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -74,6 +120,10 @@ export default {
       main: "시스템 설정 >",
       sub: "작업크기 관리",
       sizeCategory: [],
+      size: [],
+      activeCategory: false,
+      activeSize: false,
+      selected: 1,
     };
   },
   mixins: [table],
@@ -82,12 +132,25 @@ export default {
       this.$axios.get("/admin/api/size_category.php").then((response) => {
         this.sizeCategory = response.data;
         this.$endloading();
-        console.log(this.sizeCategory);
       });
+    },
+    addTemplate() {},
+    getSize() {
+      this.$axios.get("/admin/api/size.php").then((response) => {
+        this.size = response.data;
+        this.$endloading();
+      });
+    },
+    activeTemplate(i) {
+      this.activeCategory = i;
+    },
+    activeSizeName(i) {
+      this.activeSize = i;
     },
   },
   mounted() {
     this.getSizeCategory();
+    this.getSize();
   },
 };
 </script>
